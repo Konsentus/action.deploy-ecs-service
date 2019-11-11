60c4bb22ac044d70d3a525f832746c523900c5a2
odellt <tom.odell@konsentus.com>
2019-11-11 16:22:55 +0000

    :construction_worker: Adds Docker image digest check to running task

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,46 +30,108 @@ check_env_vars() {
     "AWS_REGION"
   )
 
-  for VARIABLE_NAME in "${requiredVariables[@]}"
+  for variable_name in "${requiredVariables[@]}"
   do
-    if [[ -z "${!VARIABLE_NAME}" ]]; then
-      echo "Required environment variable: ${VARIABLE_NAME} is not defined. Exiting..."
+    if [[ -z "${!variable_name}" ]]; then
+      echo "Required environment variable: $variable_name is not defined. Exiting"
       exit 3;
     fi
   done
 }
 
 assume_role() {
-  echo "Assuming role: ${AWS_ACCOUNT_ROLE} in account: ${AWS_ACCOUNT_ID}..."
-  local credentials=$(aws sts assume-role --role-arn "arn:aws:iam::${AWS_ACCOUNT_ID}:role/${AWS_ACCOUNT_ROLE}" --role-session-name docker-push --output json)
+  echo "Assuming role: $AWS_ACCOUNT_ROLE in account: $aws_account_id"
+
+  local credentials
+
+  credentials=$(aws sts assume-role --role-arn "arn:aws:iam::$aws_account_id:role/$AWS_ACCOUNT_ROLE" --role-session-name ecs-force-refresh --output json)
+  assume_role_result=$?
+
+  if [ $assume_role_result -ne 0 ]; then
+    echo "Failed to assume role $AWS_ACCOUNT_ROLE in account: $AWS_ACCOUNT_ID. Exiting"
+    exit $assume_role_result
+  fi
 
   export AWS_ACCESS_KEY_ID
   export AWS_SECRET_ACCESS_KEY
   export AWS_SESSION_TOKEN
-  export AWS_DEFAULT_REGION=${AWS_REGION}
+  export AWS_DEFAULT_REGION=$AWS_REGION
 
-  AWS_ACCESS_KEY_ID=$(jq -r .Credentials.AccessKeyId <<< ${credentials})
-  AWS_SECRET_ACCESS_KEY=$(jq -r .Credentials.SecretAccessKey <<< ${credentials})
-  AWS_SESSION_TOKEN=$(jq -r .Credentials.SessionToken <<< ${credentials})
+  AWS_ACCESS_KEY_ID=$(jq -r .Credentials.AccessKeyId <<< $credentials)
+  AWS_SECRET_ACCESS_KEY=$(jq -r .Credentials.SecretAccessKey <<< $credentials)
+  AWS_SESSION_TOKEN=$(jq -r .Credentials.SessionToken <<< $credentials)
+
   echo "Successfully assumed role"
 }
 
 deploy_service_task() {
-  aws ecs update-service --cluster ${CLUSTER_NAME} --service ${INPUT_SERVICE_NAME} --force-new-deployment
-  aws ecs wait services-stable --cluster ${CLUSTER_NAME} --services ${INPUT_SERVICE_NAME}
+  echo "Forcing deployment of the $service_name service in the $cluster_name cluster"
+
+  local service_metadata
+
+  service_metadata=$(aws ecs update-service --cluster $cluster_name --service $INPUT_SERVICE_NAME --force-new-deployment)
+
+  local exitCode=$?
+  if [ $exitCode -ne 0 ]; then
+    echo "Failed to force new deployment of the $service_name service in the $cluster_name cluster. Exiting"
+    exit $exitCode
+  fi
 }
 
-# Returns branch name
+wait_for_service_to_stabilise() {
+  echo "Waiting for the service to stabilise"
+  aws ecs wait services-stable --cluster $cluster_name --services $INPUT_SERVICE_NAME
+  local exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+    echo "Failed to wait for the stabilisation of the $service_name service in the $cluster_name cluster. Exiting"
+    exit $exit_code
+  fi
+  echo "Service has stabilised"
+}
+
+truncate_long_string() {
+  echo $1 | sed -E 's/(.{5})(.{1,})$/\1/'
+}
+
+check_task_container_digest() {
+  echo "Checking container image digests"
+  echo "Expected image digest: $INPUT_EXPECTED_IMAGE_DIGEST"
+
+  echo "Retrieving task ARNs"
+  local running_tasks=$(aws ecs list-tasks --cluster $cluster_name --service-name $service_name --desired-status RUNNING | jq .taskArns)
+
+
+  for task_arn in $(echo "$running_tasks" | jq -r '.[]'); do
+    echo "Retrieving image digest for task: $task_arn"
+    local task_image_digest=$(aws ecs describe-tasks --tasks $task_arn --cluster $cluster_name | jq -r .tasks[0].containers[0].imageDigest | cut -d : -f 2)
+
+    if [ "$task_image_digest" == "$INPUT_EXPECTED_IMAGE_DIGEST" ]; then
+      echo "The image digest for task: $task_arn matches the expected image digest"
+    else
+      echo "The image digest for task: $task_arn does not match the expected image digest: $task_image_digest"
+    fi
+  done
+}
+
+echo "Force new ECS deployment"
+
+# Get branch name
 # e.g. return "master" from "refs/heads/master"
-BRANCH_NAME=${GITHUB_REF##*/}
+branch_name=${GITHUB_REF##*/}
 
-AWS_ACCOUNT_ID=$(echo $INPUT_ENVIRONMENT_CONFIGURATION | jq -r .$BRANCH_NAME.awsAccountId)
-CLUSTER_NAME=$(echo $INPUT_ENVIRONMENT_CONFIGURATION | jq -r .$BRANCH_NAME.clusterName)
-SERVICE_NAME=${INPUT_SERVICE_NAME}
+aws_account_id=$(echo $INPUT_ENVIRONMENT_CONFIGURATION | jq -r .$branch_name.awsAccountId)
+cluster_name=$(echo $INPUT_ENVIRONMENT_CONFIGURATION | jq -r .$branch_name.clusterName)
+service_name=$INPUT_SERVICE_NAME
 
+echo "Target cluster: $cluster_name"
+echo "Target service: $service_name"
 
 check_env_vars
+
 assume_role
 
-echo "Deploying service ${SERVICE_NAME} to cluster ${CLUSTER_NAME}..."
 deploy_service_task
+
+wait_for_service_to_stabilise
+
+check_task_container_digest
